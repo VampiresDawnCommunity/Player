751a09798b96cf555f1d7c2388cfe9e433015de9
florianessl <github@iessl.at>
2024-02-28 18:16:07 +0100

    Remove dumb debug warning

--- src/game_map.cpp
+++ src/game_map.cpp
@@ -1515,8 +1515,6 @@ bool Game_Map::GetNeedRefresh() {
 	if (Player::game_config.patch_anti_lag_switch.Get() && Main_Data::game_switches->Get(Player::game_config.patch_anti_lag_switch.Get())) {
 		return false;
 	}
-	if (need_refresh)
-	Output::Warning("HUGO");
 
 	return need_refresh;
 }
